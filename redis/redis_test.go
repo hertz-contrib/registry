e8ecbe149345536355302080095914a6e49cae52
Lorain <743804605@qq.com>
2022-11-10 17:07:28 +0800

    enrich ut

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -17,10 +17,18 @@ package redis
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/cloudwego/hertz/pkg/app"
+	hzcli "github.com/cloudwego/hertz/pkg/app/client"
+	"github.com/cloudwego/hertz/pkg/app/middlewares/client/sd"
+	hzsrv "github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/app/server/registry"
+	"github.com/cloudwego/hertz/pkg/common/config"
 	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/assert"
 )
@@ -190,4 +198,52 @@ func TestNewMentor(t *testing.T) {
 	m1 := newMentor()
 	m2 := newMentor()
 	assert.Equal(t, m1, m2)
+}
+
+// TestRedisRegistryWithHertz Test redis registry complete workflow (service registry|service de-registry|service resolver) with hertz.
+func TestRedisRegistryWithHertz(t *testing.T) {
+	addr := "127.0.0.1:8080"
+	redisAddr := "127.0.0.1:6379"
+	srvName := "hertz.with.registry"
+	r := NewRedisRegistry(redisAddr)
+	h := hzsrv.Default(
+		hzsrv.WithHostPorts(addr),
+		hzsrv.WithRegistry(r, &registry.Info{
+			ServiceName: srvName,
+			Addr:        utils.NewNetAddr("tcp", addr),
+			Weight:      10,
+			Tags:        nil,
+		}),
+	)
+	h.GET("/ping", func(_ context.Context, ctx *app.RequestContext) {
+		ctx.JSON(consts.StatusOK, utils.H{"ping": "pong"})
+	})
+	go h.Spin()
+	time.Sleep(4 * time.Second)
+
+	hc, _ := hzcli.NewClient()
+	resolver := NewRedisResolver(redisAddr)
+	hc.Use(sd.Discovery(resolver))
+
+	url := fmt.Sprintf("http://%v/ping", srvName)
+	status, body, err := hc.Get(context.Background(), nil, url, config.WithSD(true))
+	assert.Nil(t, err)
+	assert.Equal(t, 200, status)
+	assert.Equal(t, "{\"ping\":\"pong\"}", string(body))
+
+	opts := h.GetOptions()
+	assert.Equal(t, opts.RegistryInfo.ServiceName, srvName)
+	assert.Equal(t, opts.RegistryInfo.Addr.String(), addr)
+	assert.Equal(t, opts.RegistryInfo.Weight, 10)
+	assert.Nil(t, opts.RegistryInfo.Tags)
+
+	if err := h.Shutdown(context.Background()); err != nil {
+		t.Errorf("HERTZ: Shutdown error=%v", err)
+	}
+	time.Sleep(5 * time.Second)
+
+	status2, body2, err2 := hc.Get(context.Background(), nil, url, config.WithSD(true))
+	assert.True(t, err2 != nil)
+	assert.Equal(t, 0, status2)
+	assert.Equal(t, "", string(body2))
 }

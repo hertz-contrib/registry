c502ef53d9e21fe3cf6289653f94531b0640fc37
清 <claude-zq@qq.com>
2023-01-09 11:34:27 +0800

    feat: add custom-config example for consul

---
+++ consul/example/custom-config/client/main.go
@@ -0,0 +1,122 @@
+// Copyright 2022 CloudWeGo Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/cloudwego/hertz/pkg/app/client"
+	"github.com/cloudwego/hertz/pkg/app/client/discovery"
+	"github.com/cloudwego/hertz/pkg/app/client/loadbalance"
+	"github.com/cloudwego/hertz/pkg/app/middlewares/client/sd"
+	"github.com/cloudwego/hertz/pkg/common/config"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/hashicorp/consul/api"
+	"github.com/hertz-contrib/registry/consul"
+)
+
+func main() {
+	// build a consul client
+	consulConfig := api.DefaultConfig()
+	consulConfig.Address = "127.0.0.1:8500"
+	consulClient, err := api.NewClient(consulConfig)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	// build a consul resolver with the consul client
+	r := consul.NewConsulResolver(consulClient)
+
+	discoveryWithSD(r)
+	discoveryWithTag(r)
+	discoveryWithCustomizedAddr(r)
+	discoveryWithLoadBalanceOptions(r)
+}
+
+func discoveryWithSD(r discovery.Resolver) {
+	fmt.Println("simply discovery:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithTag(r discovery.Resolver) {
+	fmt.Println("discovery with tag:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true), config.WithTag("key1", "val1"))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithCustomizedAddr(r discovery.Resolver) {
+	fmt.Println("discovery with customizedAddr:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+
+	localIP, err := consul.GetLocalIPv4Address()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cli.Use(sd.Discovery(r, sd.WithCustomizedAddrs(fmt.Sprintf("%s:5001", localIP))))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true), config.WithTag("key1", "val1"))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithLoadBalanceOptions(r discovery.Resolver) {
+	fmt.Println("discovery with loadBalanceOptions:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r, sd.WithLoadBalanceOptions(loadbalance.NewWeightedBalancer(), loadbalance.Options{
+		RefreshInterval: 5 * time.Second,
+		ExpireInterval:  15 * time.Second,
+	})))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
---
+++ consul/example/custom-config/server/main.go
@@ -0,0 +1,99 @@
+// Copyright 2022 CloudWeGo Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/app/server/registry"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/hashicorp/consul/api"
+	"github.com/hertz-contrib/registry/consul"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	config := api.DefaultConfig()
+	config.Address = "127.0.0.1:8500"
+	consulClient, err := api.NewClient(config)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// custom check
+	check := &api.AgentServiceCheck{
+		Interval:                       "7s",
+		Timeout:                        "5s",
+		DeregisterCriticalServiceAfter: "15s",
+	}
+	r := consul.NewConsulRegister(consulClient, consul.WithCheck(check))
+
+	localIP, err := consul.GetLocalIPv4Address()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		addr := fmt.Sprintf("%s:%d", localIP, 5001)
+
+		h := server.Default(
+			server.WithHostPorts(addr),
+			server.WithRegistry(r, &registry.Info{
+				ServiceName: "custom-config-demo",
+				Addr:        utils.NewNetAddr("tcp", addr),
+				Weight:      10,
+				Tags: map[string]string{
+					"key1": "val1",
+				},
+			}),
+		)
+		h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+			ctx.JSON(consts.StatusOK, utils.H{"ping": "pong1"})
+		})
+		h.Spin()
+	}()
+
+	go func() {
+		defer wg.Done()
+		addr := fmt.Sprintf("%s:%d", localIP, 5002)
+		h := server.Default(
+			server.WithHostPorts(addr),
+			server.WithRegistry(r, &registry.Info{
+				ServiceName: "custom-config-demo",
+				Addr:        utils.NewNetAddr("tcp", addr),
+				Weight:      10,
+				Tags: map[string]string{
+					"key2": "val2",
+				},
+			}),
+		)
+		h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+			ctx.JSON(consts.StatusOK, utils.H{"ping": "pong2"})
+		})
+		h.Spin()
+	}()
+
+	wg.Wait()
+}

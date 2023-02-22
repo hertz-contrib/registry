a07b41a15fb498151f37a473612ef05c3db43069
a631807682 <631807682@qq.com>
2023-02-22 15:52:51 +0800

    feat: add more example

---
+++ servicecomb/example/custom_config/client/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/cloudwego/hertz/pkg/app/client"
+	"github.com/cloudwego/hertz/pkg/app/client/discovery"
+	"github.com/cloudwego/hertz/pkg/app/client/loadbalance"
+	"github.com/cloudwego/hertz/pkg/app/middlewares/client/sd"
+	"github.com/cloudwego/hertz/pkg/common/config"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/hertz/pkg/protocol"
+	"github.com/go-chassis/sc-client"
+	"github.com/hertz-contrib/registry/servicecomb"
+)
+
+type Example struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
+const scAddr = "127.0.0.1:30100"
+
+func main() {
+	scClient, err := sc.NewClient(sc.Options{
+		Endpoints:  []string{scAddr},
+		Timeout:    5 * time.Second,
+		Compressed: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	r := servicecomb.NewSCResolver(scClient)
+	discoveryWithSD(r)
+	discoveryWithTag(r)
+	discoveryWithCustomizedAddr(r)
+	discoveryWithLoadBalanceOptions(r)
+	discoveryThenUsePostMethod(r)
+}
+
+func discoveryWithSD(r discovery.Resolver) {
+	hlog.Info("simply discovery:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+
+	cli.Use(sd.Discovery(r))
+
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithTag(r discovery.Resolver) {
+	hlog.Info("discovery with tag:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true), config.WithTag("key1", "val1"))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithCustomizedAddr(r discovery.Resolver) {
+	hlog.Info("discovery with customizedAddr:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+
+	cli.Use(sd.Discovery(r, sd.WithCustomizedAddrs("127.0.0.1:8702")))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true), config.WithTag("key1", "val1"))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithLoadBalanceOptions(r discovery.Resolver) {
+	hlog.Info("discovery with loadBalanceOptions:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r, sd.WithLoadBalanceOptions(loadbalance.NewWeightedBalancer(), loadbalance.Options{
+		RefreshInterval: 5 * time.Second,
+		ExpireInterval:  15 * time.Second,
+	})))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryThenUsePostMethod(r discovery.Resolver) {
+	hlog.Info("discovery and use post method to send request:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r))
+
+	for i := 0; i < 10; i++ {
+		// set request config、method、request uri.
+		req := protocol.AcquireRequest()
+		req.SetOptions(config.WithSD(true))
+		req.SetMethod("POST")
+		req.SetRequestURI("http://custom-config-demo/ping")
+		t := Example{A: i, B: i}
+		bytes, _ := json.Marshal(t)
+		// set body and content type
+		req.SetBody(bytes)
+		req.Header.SetContentTypeBytes([]byte("application/json"))
+		resp := protocol.AcquireResponse()
+		// send request
+		err := cli.Do(context.Background(), req, resp)
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", resp.StatusCode(), string(resp.Body()))
+	}
+}
--- servicecomb/go.mod
+++ servicecomb/go.mod
@@ -14,7 +14,6 @@ require (
 	github.com/bytedance/go-tagexpr/v2 v2.9.2 // indirect
 	github.com/bytedance/gopkg v0.0.0-20220531084716-665b4f21126f // indirect
 	github.com/bytedance/sonic v1.3.0 // indirect
-	github.com/cenkalti/backoff v2.0.0+incompatible // indirect
 	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20211019084208-fb5309c8db06 // indirect
 	github.com/cloudwego/netpoll v0.2.5 // indirect
@@ -24,6 +23,7 @@ require (
 	github.com/go-chassis/openlog v1.1.2 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/henrylee2cn/ameda v1.4.10 // indirect
 	github.com/henrylee2cn/goutil v0.0.0-20210127050712-89660552f6f8 // indirect
@@ -37,9 +37,8 @@ require (
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20220412001346-fc48f9fe4c15 // indirect
-	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )

fa9cbae8a98d8d66c4c6dc309d264a19a6f8da5b
MulL1on <liangweijian666@outlook.com>
2023-04-22 16:15:52 +0800

    fix: fix the module's path and fix the import path in example and README

--- eureka/example/client/main.go
+++ eureka/example/client/main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/cloudwego/hertz/pkg/app/middlewares/client/sd"
 	"github.com/cloudwego/hertz/pkg/common/config"
 	"github.com/cloudwego/hertz/pkg/common/hlog"
-	"github.com/hertz-contrib/eureka"
+	"github.com/hertz-contrib/registry/eureka"
 )
 
 func main() {

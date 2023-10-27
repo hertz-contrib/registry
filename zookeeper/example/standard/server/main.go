133a1ea5b0d078bbd9b76997ffa3706edb1f2557
Viola_Pioggia <114391708+ViolaPioggia@users.noreply.github.com>
2023-10-27 20:50:24 +0800

    feat:add more zookeeper examples (#92)
    * feat:add more zookeeper examples
    
    * style:gofumpt

--- zookeeper/example/standard/server/main.go
+++ zookeeper/example/standard/server/main.go
@@ -16,6 +16,8 @@ package main
 
 import (
 	"context"
+	"net"
+	"sync"
 	"time"
 
 	"github.com/cloudwego/hertz/pkg/app"
@@ -26,22 +28,70 @@ import (
 	"github.com/hertz-contrib/registry/zookeeper"
 )
 
+var wg sync.WaitGroup
+
+type Example struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
 func main() {
-	addr := "127.0.0.1:8888"
 	r, err := zookeeper.NewZookeeperRegistry([]string{"127.0.0.1:2181"}, 40*time.Second)
 	if err != nil {
 		panic(err)
 	}
-	h := server.Default(
-		server.WithHostPorts(addr),
-		server.WithRegistry(r, &registry.Info{
-			ServiceName: "hertz.test.demo",
-			Addr:        utils.NewNetAddr("tcp", addr),
-			Weight:      10,
-			Tags:        nil,
-		}))
-	h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
-		ctx.JSON(consts.StatusOK, utils.H{"ping": "pong2"})
-	})
-	h.Spin()
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		addr := net.JoinHostPort("127.0.0.1", "8888")
+		tags := map[string]string{"group": "blue", "idc": "hd1"}
+		h := server.Default(
+			server.WithHostPorts(addr),
+			server.WithRegistry(r, &registry.Info{
+				ServiceName: "hertz.test.demo1",
+				Addr:        utils.NewNetAddr("tcp", addr),
+				Weight:      10,
+				Tags:        tags,
+			}))
+		h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+			ctx.JSON(consts.StatusOK, utils.H{"ping": "pong1"})
+		})
+		h.POST("/ping", func(c context.Context, ctx *app.RequestContext) {
+			e := Example{}
+			if err := ctx.Bind(&e); err != nil {
+				ctx.String(consts.StatusBadRequest, err.Error())
+				return
+			}
+			ctx.JSON(consts.StatusOK, e)
+		})
+		h.Spin()
+	}()
+	go func() {
+		defer wg.Done()
+		addr := net.JoinHostPort("127.0.0.1", "8889")
+		tags := map[string]string{"group": "red", "idc": "hd2"}
+		h := server.Default(
+			server.WithHostPorts(addr),
+			server.WithRegistry(r, &registry.Info{
+				ServiceName: "hertz.test.demo2",
+				Addr:        utils.NewNetAddr("tcp", addr),
+				Weight:      10,
+				Tags:        tags,
+			}))
+		h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+			ctx.JSON(consts.StatusOK, utils.H{"ping": "pong2"})
+		})
+		h.POST("/ping", func(c context.Context, ctx *app.RequestContext) {
+			e := Example{}
+			if err := ctx.Bind(&e); err != nil {
+				ctx.String(consts.StatusBadRequest, err.Error())
+				return
+			}
+			ctx.JSON(consts.StatusOK, e)
+		})
+		h.Spin()
+	}()
+
+	wg.Wait()
 }

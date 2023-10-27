f486e0e6f4b3eaecb45e7140906de2038a08724b
violapioggia <2604296771@qq.com>
2023-10-27 11:22:41 +0800

    feat:add more zookeeper examples

--- zookeeper/example/standard/client/main.go
+++ zookeeper/example/standard/client/main.go
@@ -16,6 +16,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/cloudwego/hertz/pkg/app/client/discovery"
+	"github.com/cloudwego/hertz/pkg/app/client/loadbalance"
+	"github.com/cloudwego/hertz/pkg/protocol"
+	"net"
 	"time"
 
 	"github.com/cloudwego/hertz/pkg/app/client"
@@ -25,21 +31,116 @@ import (
 	"github.com/hertz-contrib/registry/zookeeper"
 )
 
+type Example struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
 func main() {
-	cli, err := client.NewClient()
+	r, err := zookeeper.NewZookeeperResolver([]string{"127.0.0.1:2181"}, 40*time.Second)
 	if err != nil {
 		panic(err)
 	}
-	r, err := zookeeper.NewZookeeperResolver([]string{"127.0.0.1:2181"}, 40*time.Second)
+	discoveryWithSD(r)
+	discoveryWithTag(r)
+	discoveryWithCustomizedAddr(r)
+	discoveryWithLoadBalanceOptions(r)
+	discoveryThenUsePostMethod(r)
+}
+
+func discoveryWithSD(r discovery.Resolver) {
+	fmt.Println("simply discovery:")
+	cli, err := client.NewClient()
 	if err != nil {
 		panic(err)
 	}
 	cli.Use(sd.Discovery(r))
 	for i := 0; i < 10; i++ {
-		status, body, err := cli.Get(context.Background(), nil, "http://hertz.test.demo/ping", config.WithSD(true))
+		status, body, err := cli.Get(context.Background(), nil, "http://hertz.test.demo1/ping", config.WithSD(true))
 		if err != nil {
 			hlog.Fatal(err)
 		}
 		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithTag(r discovery.Resolver) {
+	fmt.Println("discovery with tag:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://hertz.test.demo1/ping", config.WithSD(true), config.WithTag("key1", "val1"))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithCustomizedAddr(r discovery.Resolver) {
+	fmt.Println("discovery with customizedAddr:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+
+	cli.Use(sd.Discovery(r, sd.WithCustomizedAddrs(net.JoinHostPort("127.0.0.1", "8888"))))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://custom-config-demo/ping", config.WithSD(true), config.WithTag("key1", "val1"))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryWithLoadBalanceOptions(r discovery.Resolver) {
+	fmt.Println("discovery with loadBalanceOptions:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r, sd.WithLoadBalanceOptions(loadbalance.NewWeightedBalancer(), loadbalance.Options{
+		RefreshInterval: 5 * time.Second,
+		ExpireInterval:  15 * time.Second,
+	})))
+	for i := 0; i < 10; i++ {
+		status, body, err := cli.Get(context.Background(), nil, "http://hertz.test.demo1/ping", config.WithSD(true))
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", status, string(body))
+	}
+}
+
+func discoveryThenUsePostMethod(r discovery.Resolver) {
+	fmt.Println("discovery and use post method to send request:")
+	cli, err := client.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	cli.Use(sd.Discovery(r))
+
+	for i := 0; i < 10; i++ {
+		// set request config、method、request uri.
+		req := protocol.AcquireRequest()
+		req.SetOptions(config.WithSD(true))
+		req.SetMethod("POST")
+		req.SetRequestURI("http://hertz.test.demo1/ping")
+		t := Example{A: i, B: i}
+		bytes, _ := json.Marshal(t)
+		// set body and content type
+		req.SetBody(bytes)
+		req.Header.SetContentTypeBytes([]byte("application/json"))
+		resp := protocol.AcquireResponse()
+		// send request
+		err := cli.Do(context.Background(), req, resp)
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s", resp.StatusCode(), string(resp.Body()))
 	}
 }
